a1495e9f6c61f71e50b6aa27449335ebb6ba382f
Archimboldi <1751844336@qq.com>
2020-11-16 14:55:24 +0800

    p

--- app/src/main/shebei.js
+++ app/src/main/shebei.js
@@ -1,7 +1,8 @@
 import { useQuery, gql } from '@apollo/client';
+import React from 'react';
 import { Table } from 'rsuite';
 
-const { Column, HeaderCell, Cell, Pagination } = Table;
+const { Column, HeaderCell, Cell } = Table;
 
 const EXCHANGE_RATES = gql`
   query {shebeis {
@@ -17,21 +18,48 @@ const EXCHANGE_RATES = gql`
   }}
 `;
 
-const Shebei=()=> {
-    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+class PaginationTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayLength: 10,
+      loading: false,
+      page: 1
+    };
+    this.Data = props.data;
+    this.handleChangePage = this.handleChangePage.bind(this);
+    this.handleChangeLength = this.handleChangeLength.bind(this);
+  }
+  handleChangePage(dataKey) {
+    this.setState({
+      page: dataKey
+    });
+  }
+  handleChangeLength(dataKey) {
+    this.setState({
+      page: 1,
+      displayLength: dataKey
+    });
+  }
+  getData() {
+    const { displayLength, page } = this.state;
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-        console.log(data)
-    return(
-    <div>
-        <Table
-            height={1000}
-            data={data.shebeis}
-            onRowClick={data => {
-                console.log(data);
-            }}>
-            <Column width={100} align="center" fixed>
+    return this.Data.filter((v, i) => {
+      const start = displayLength * (page - 1);
+      const end = start + displayLength;
+      return i >= start && i < end;
+    });
+  }
+  render() {
+    const data = this.getData();
+    const { loading, displayLength, page } = this.state;
+
+      return (
+        <div>
+          <Table height={420} data={data} loading={loading} onRowClick={data => {
+          console.log(data);
+        }}>
+          <Column width={100} align="center" fixed>
                 <HeaderCell>资产编号</HeaderCell>
                 <Cell dataKey="zcbh" />    
             </Column>
@@ -67,9 +95,51 @@ const Shebei=()=> {
                 <HeaderCell>备注</HeaderCell>
                 <Cell dataKey="sbbz" />    
             </Column>
-       </Table>
-    </div>
+          </Table>
+  
+          <Table.Pagination
+            lengthMenu={[
+              {
+                value: 10,
+                label: 10
+              },
+              {
+                value: 20,
+                label: 20
+              },
+              {
+                value: 30,
+                label: 30
+              },
+              {
+                value: 50,
+                label: 50
+              },
+              {
+                value: 100,
+                label: 100
+              }
+            ]}
+            activePage={page}
+            displayLength={displayLength}
+            total={this.Data.length}
+            onChangePage={this.handleChangePage}
+            onChangeLength={this.handleChangeLength}
+          />
+        </div>
+      );
+    }
+  }
+function Shebei() {
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return(
+    <div><PaginationTable data={data.shebeis} /></div>
     )
+
 }
 
 export default Shebei;
